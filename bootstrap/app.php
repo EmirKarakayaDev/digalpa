8d164a39cd5fa0c1eaf4f0a7580373e5069c8fab
Emir Karakaya <dev.emirkarakaya@gmail.com>
2026-07-14 17:41:22 +0300

    TrustProxies icin gecersiz header sabitini kaldir

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,8 +4,6 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
-use Illuminate\Http\Middleware\TrustProxies;
-
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
@@ -16,10 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
         // Railway (ve çoğu PaaS) SSL'i kendi proxy katmanında sonlandırıp
         // uygulamaya düz HTTP ile ulaşıyor; X-Forwarded-* başlıklarına
         // güvenmeden Laravel asset/URL linklerini yanlışlıkla http:// üretiyordu.
-        $middleware->trustProxies(at: '*', headers: TrustProxies::HEADER_X_FORWARDED_FOR
-            | TrustProxies::HEADER_X_FORWARDED_HOST
-            | TrustProxies::HEADER_X_FORWARDED_PORT
-            | TrustProxies::HEADER_X_FORWARDED_PROTO);
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(

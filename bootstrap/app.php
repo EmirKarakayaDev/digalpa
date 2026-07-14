e0d809ffe12a060e3d3cececad5cef6b1744aed3
Emir Karakaya <dev.emirkarakaya@gmail.com>
2026-07-14 17:36:58 +0300

    Reverse proxy arkasinda (Railway vb.) HTTPS URL uretimi icin TrustProxies ekle

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Request;
+use Illuminate\Http\Middleware\TrustProxies;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -12,7 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
-        //
+        // Railway (ve çoğu PaaS) SSL'i kendi proxy katmanında sonlandırıp
+        // uygulamaya düz HTTP ile ulaşıyor; X-Forwarded-* başlıklarına
+        // güvenmeden Laravel asset/URL linklerini yanlışlıkla http:// üretiyordu.
+        $middleware->trustProxies(at: '*', headers: TrustProxies::HEADER_X_FORWARDED_FOR
+            | TrustProxies::HEADER_X_FORWARDED_HOST
+            | TrustProxies::HEADER_X_FORWARDED_PORT
+            | TrustProxies::HEADER_X_FORWARDED_PROTO);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
